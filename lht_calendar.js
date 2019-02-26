75585c983f3daa4b04e174b9f1a0578a32cd00f6
Dylan Kelley <dkelley989@west-mec.org>
2019-02-26 15:24:41 -0700

    Lesson changes
    Created a function to write the table rows for each day of the month by determining the starting day of the month and writing blank cells preceding the staring day, as well as writing cells for each day of the month.

--- lht_calendar.js
+++ lht_calendar.js
@@ -31,7 +31,7 @@
 */
 
 /* Set the date displayed in the calendar */
-var thisDay = new Date("August 24, 2018");
+var thisDay = new Date();
 
 // Write the calendar to the element with the id "calendar"
 document.getElementById("calendar").innerHTML = createCalendar(thisDay);
@@ -41,6 +41,8 @@ function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
       calendarHTML += calCaption(calDate);
       calendarHTML += calWeekdayRow();
+      calendarHTML += daysInMonth(calDate);
+      calendarHTML += callDays(calDate);
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -82,7 +84,7 @@ function daysInMonth(calDate) {
 
       // Extract the four digit year and month value
       var thisYear = calDate.getFullYear();
-      var thisMonth = cal.Date.getMonth();
+      var thisMonth = calDate.getMonth();
 
       // Revise the days in February for leap years
       if (thisYear % 4 === 0) {
@@ -92,6 +94,60 @@ function daysInMonth(calDate) {
       // Return the number of days for the current month
       return dayCount[thisMonth];
 }
+
+// Function to write the table rows for each day of the month
+function callDays(calDate) {
+      // Determine the starting day of the month
+      var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+      var weekDay = day.getDay();
+
+      // Write blank cells preceding the staring day
+      var htmlCode = "<tr>";
+      for (var i = 0; i < weekDay; i++) {
+            htmlCode += "<td></td>"
+      }
+
+      // Write cells for each day of the month
+      var totalDays = daysInMonth(calDate);
+      var highlightDay = calDate.getDate();
+      for (var i = 1; i <= totalDays; i++) {
+            day.setDate(i);
+            weekDay = day.getDay();
+
+            if (weekDay === 0) {
+                  htmlCode += "<tr>";
+                  htmlCode += "<td class='calendar_dates'>" + i + "</td>";
+            }
+
+            if (i === highlightDay) {
+                  htmlCode += "<td class='caledar_dates' id='calendar_today'>" + i + "</td>";
+            } else {
+                  htmlCode += "<td class='calendar_dates'>" + i + "</td>";
+            }
+
+            if (weekDay === 6) {
+                  htmlCode += "</tr>";
+            }
+      }
+      return htmlCode;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Practice
 
 var scores = [92, 68, 83, 95, 91, 65, 77];
 
@@ -106,5 +162,9 @@ function gradeA(value) {
 var nums = ["four", "five", "five", "four", "five", "four"];
 
 for (var i = 0; i < nums.length; i++); {
-
+      if (nums[i] === "four") {
+            nums[i] = 4;
+      } else if (nums[i] === "five") {
+            nums[i] = 5;
+      }
 }
